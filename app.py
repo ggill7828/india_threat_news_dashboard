8ff7871b5faa76138d1930eafca88ebf09e379de
ggill7828 <ggill7828@gmail.com>
2025-06-17 15:00:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,6 @@ def export_news():
     return send_file(filepath, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
